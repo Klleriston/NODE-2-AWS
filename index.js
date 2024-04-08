a455fcac62dd5b01c18d270a5fa1560d65dcb44c
Klleriston <Klleandrade@outlook.com>
2024-04-08 20:57:35 -0300

    aws

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ import app from "./src/app.js";
 app.listen(process.env.PORT, () => {
     console.log(`Listen on http://localhost:${process.env.PORT}`);
 });
+
+export const handler = async (event, context) => {
+    return app(event, context);
+};
